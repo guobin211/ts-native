6574c4c16f39391c4d22fd4c086a99baa3c70e0d
michaelbguo <michaelbguo@tencent.com>
2024-12-17 22:41:28 +0800

    feat: update deps

--- src/main.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const reflect = @import("reflect.zig");
-
-pub fn main() !void {
-    std.debug.print("ts-native-zig\n", .{});
-    reflect.init_reflect();
-    reflect.pointer();
-}
