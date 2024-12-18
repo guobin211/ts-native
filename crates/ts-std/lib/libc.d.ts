ffee8c5149d2592a58257140477564583ef68233
michaelbguo <michaelbguo@tencent.com>
2024-12-18 14:52:47 +0800

    feat: add pest

--- crates/ts-std/lib/libc.d.ts
+++ crates/ts-std/lib/libc.d.ts
@@ -1,11 +1,38 @@
-interface Stdio {
+export interface Stdio {
     /**
      * 打印输出
      */
     printf: (arg: string) => void;
 }
 
+export interface Stdlib {
+    exit(code: number): void;
+}
+
+export interface Time {
+    time(t?: number | null): number;
+}
+
+export interface Math {
+    cos(angle: number): number;
+}
+
 /**
  * 标准输入输出
  */
 export declare const stdio: Stdio;
+
+/**
+ * 操作日期和时间
+ */
+export declare const time: Time;
+
+/**
+ * 通用工具函数
+ */
+export declare const stdlib: Stdlib;
+
+/**
+ * 数学函数
+ */
+export declare const math: Math;
